22a0ca323bd24a9e9c0ed471a0b04e3f90a8d9ca
amandell2 <amanda.mandell2@gmail.com>
2022-03-31 19:13:41 -0400

    completed extended challenge

--- react-exercise-5/src/components/ArraysEx.tsx
+++ react-exercise-5/src/components/ArraysEx.tsx
@@ -14,10 +14,11 @@ function ArraysEx(){
     }
 
     const [pets, setPets]=useState([
-        {name: "Nala", type: "Pitbull", id: 1},
-        {name: "Ludo", type: "Pitbull", id: 2},
-        {name: "Rocky", type: "Chocolate Lab", id: 3}
+        {name: "Nala", type: "Dog", id: 1},
+        {name: "Ludo", type: "Parrot", id: 2},
+        {name: "Rocky", type: "Iguana", id: 3}
     ])
+        
     
     return(
        <div className="ArraysEx">
@@ -41,9 +42,14 @@ function ArraysEx(){
                 <tr>
                     <th>Name</th>
                     <th>Type</th>
+                    <th>Action</th>
                 </tr>
                 <tbody className="Pets-row">
-                    {pets.map((pet, i)=> <tr key = {i}><td>{pet.name}</td> <td>{pet.type}</td></tr>)}
+                    {pets.map((pet, i)=> <tr key = {i}>
+                        <td>{pet.name}</td> 
+                        <td>{pet.type}</td>
+                        <td><button onClick = {() => setPets(prev => [...prev.slice(0,i), ... prev.slice(i +1)])}>Delete</button></td>
+                    </tr>)}
                 </tbody>
             </table>
        </section>
